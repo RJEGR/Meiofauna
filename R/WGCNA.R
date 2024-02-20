d190d6b80da6b37499fd9763d19bbd04d2363fd7
Ricardo Gore <rjhgore@gmail.com>
2024-02-20 15:15:38 -0500

    Create WGCNA.R

---
+++ R/WGCNA.R
@@ -0,0 +1,3 @@
+# GENERATE NETWORK ANALYSIS USING WGCNA
+# EX. https://www.polarmicrobes.org/weighted-gene-correlation-network-analysis-wgcna-applied-to-microbial-communities/
+# use also Sparse Estimation of Correlations among Microbiomes (SECOM) for correlation analysis. 
