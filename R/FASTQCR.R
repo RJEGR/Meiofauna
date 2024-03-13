0db1234ac4c293b1a2d7462cd1464b2d58db7faa
Ricardo Gore <rjhgore@gmail.com>
2024-03-13 08:48:00 -0500

    Create FASTQCR.R

---
+++ R/FASTQCR.R
@@ -0,0 +1,8 @@
+# test fasqtrc
+# https://github.com/RJEGR/infovis/blob/a7e929a9fc8694dd981835c6ca4bf61110a1ca63/qualMaxEEplot.R#L15
+# https://github.com/RJEGR/templates/blob/4a54e300ea346d28b8c5a14005480dd8a141057a/templado-informe-18S-2020-M4.Rmd#L220
+
+# if(!require(devtools)) install.packages("devtools")
+# devtools::install_github("kassambara/fastqcr")
+
+library(fastqcr)
